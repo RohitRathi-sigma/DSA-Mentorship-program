4feb1889c391c7fdf64c7ffcf744f86dc88e3ebc
RohitRathi-sigma <rohitrathi9588@gmail.com>
2025-07-13 22:51:45 +0530

    Print advanced pattern

---
+++ PATTERN PRINTING IN C++/printAdvPattern.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    int row=1;
+
+    while(row<=n){
+        //Print space (1st triangle)
+        int space=n-row;
+        while(space){
+            cout<<" ";
+            space=space-1;
+        }
+        
+        //Print 2nd triangle 
+        int j=1;
+        while(j<=row){
+            cout<<j;
+            j=j+1;
+        }
+        
+        //Prinr 3rd triangle
+        int start=row-1;
+        while(start){
+            cout<<start;
+            start=start-1;
+        }
+        cout<<endl;
+        row=row+1;
+
+    }
+
+}
